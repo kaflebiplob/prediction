5134a9c2bf031894130c8a46acdb2eb6ea38b141
Biplob kafle <biplobkafle21@gmail.com>
2024-08-04 09:20:21 +0545

    first commit

---
+++ src/app/prediction-name/[name]/page.tsx
@@ -0,0 +1,48 @@
+interface params {
+  params: {
+    name: string;
+  };
+}
+
+const getPredictedAge = async (name: string) => {
+  const response = await fetch(`https://api.agify.io/?name=${name}`);
+  return response.json();
+};
+const getPredictedGender = async (name: string) => {
+  const response = await fetch(`https://api.genderize.io/?name=${name}`);
+ 
+  return response.json();
+};
+const getPredictedNationality = async (name: string) => {
+  const response = await fetch(`https://api.nationalize.io/?name=${name}`);
+  return response.json();
+};
+export default async function PageName({ params }: any) {
+  const getage = getPredictedAge(params.name)
+  const getgender= getPredictedGender(params.name)
+  const getnational = getPredictedNationality(params.name)
+
+  const[age,gender,nationality]= await Promise.all([
+    getage,
+    getgender,
+    getnational
+  ])
+  return( <div>
+    <div>
+      {params.name}
+    </div>
+    <div>
+     Age:{age?.age}
+    </div>
+    <div>{params.age}</div>
+    <div>
+      Gender:{gender?.gender}
+    </div>
+    <div>
+      Country:{nationality?.country?.[0]?.country_id}
+    </div>
+  
+  
+  </div>
+  )
+}
