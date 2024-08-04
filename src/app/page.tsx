0036440cca08dc7ff4bed6ff369429845222848c
Biplob kafle <biplobkafle21@gmail.com>
2024-08-04 16:20:40 +0545

    all style done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,17 +16,21 @@ export default function Home() {
   
   }
   return (
-    <div>
-    <h1>Prediction Information</h1>
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="">Enter your name</label>
+    <div className="flex flex-col items-center justify-center">
+    <h1 className="mb-4">Prediction Information</h1>
+    <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center">
+      <label htmlFor="" className="mb-2">Enter your name</label>
+      <div className="">
+
       <input
         type="text"
         placeholder="Enter your name"
         value={inpValue}
         onChange={(e) => setInpValue(e.target.value)}
-      />
-      <button type="submit">submit</button>
+        className="border-yellow-200 border-2  p-1"
+        />
+      <button type="submit" className="p-1 border-yellow-200 border-2 m-1 bg-black text-white">submit</button>
+        </div>
     </form>
   </div>
   );
